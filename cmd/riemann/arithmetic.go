da5a9be52df189bc9bfe7cbdaef6f8b96a44df49
JUN JIE NAN <nanjunjie@gmail.com>
2019-07-16 23:11:39 +0800

    Refactory

--- cmd/riemann/arithmetic.go
+++ cmd/riemann/arithmetic.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
 	"io/ioutil"
-	"math/rand"
 	"os"
 
-	"github.com/jung-kurt/gofpdf"
 	"github.com/spf13/cobra"
 )
 
@@ -21,6 +17,7 @@ func init() {
 	aFlags := arithCmd.Flags()
 	aFlags.Int("degree", 50, "degree of difficulty, 0 easiest, 100 most difficult, default 50")
 	aFlags.Int("pages", 1, "number of pages to print out, default 10")
+	aFlags.Int("maximum", 100, "maximum of number, default 100")
 	RootCmd.AddCommand(arithCmd)
 }
 
@@ -29,7 +26,7 @@ func ArithmeticE(cmd *cobra.Command, args []string) (err error) {
 	if err != nil {
 		return
 	}
-	level = level % 100
+	level = level % 101
 	level = 100 - level
 	pages, err := cmd.Flags().GetInt("pages")
 	if err != nil {
@@ -38,98 +35,16 @@ func ArithmeticE(cmd *cobra.Command, args []string) (err error) {
 	if pages <= 0 {
 		pages = 10
 	}
-	pdf := gofpdf.New("P", "mm", "A4", "")
-	fn := "mcust"
-	pdf.AddUTF8FontFromBytes(fn, "", FontBytes())
-	pdf.SetLeftMargin(12.5)
-	pdf.SetTopMargin(7.5)
-	carry := func(a, b int) bool {
-		a = a % 10
-		b = b % 10
-		if a+b >= 10 {
-			return true
-		}
-		return false
+	maximum, err := cmd.Flags().GetInt("maximum")
+	if err != nil {
+		return
 	}
-	borrow := func(a, b int) bool {
-		a = a % 10
-		b = b % 10
-		if a-b < 0 {
-			return true
-		}
-		return false
+	if maximum <= 0 {
+		maximum = 100
 	}
-	options := gofpdf.ImageOptions{ImageType: "png", ReadDpi: true, AllowNegativePosition: false}
-	pdf.RegisterImageOptionsReader("riemann", options, bytes.NewBuffer(res["riemann"]))
-	pdf.SetHeaderFuncMode(func() {
-		pdf.SetFont(fn, "", 16)
-		pdf.Cell(44, 10, "姓名：")
-		pdf.Cell(44, 10, "日期：")
-		pdf.Cell(44, 10, "时间：")
-		pdf.Cell(44, 10, "分数：")
-		pdf.ImageOptions("riemann", -1, 6, 10, 10, false, options, 0, "")
-		width, _ := pdf.GetPageSize()
-		pdf.Line(10, 20, width-10, 20)
-		pdf.SetFont("Courier", "", 16)
-	}, true)
-	pdf.SetFooterFunc(func() {
-		pdf.SetY(-15)
-		pdf.SetFont("times", "I", 8)
-		width, _ := pdf.GetPageSize()
-		pdf.SetX(width/2 - 5)
-		pdf.Cell(10, 10, fmt.Sprintf("%-2d", pdf.PageNo()))
-	})
-	ops := map[int]string{0: "+", 1: "-"}
-	for p := 0; p < pages; p++ {
-		pdf.AddPage()
-		pdf.Ln(3.5)
-		easiers := 0
-		for i := 0; i < 100; i++ {
-			var a, b, c, o int
-			for {
-				a = rand.Intn(100)
-				b = rand.Intn(100)
-				if a < 10 || b < 10 {
-					easiers++
-					if easiers >= level {
-						continue
-					}
-				}
-
-				c = 0
-				o = rand.Intn(2)
-				if o == 0 {
-					c = a + b
-					if c > 100 {
-						continue
-					}
-					if !carry(a, b) {
-						easiers++
-						if easiers >= level {
-							continue
-						}
-					}
-				} else {
-					c = a - b
-					if c < 0 {
-						continue
-					}
-					if !borrow(a, b) {
-						easiers++
-						if easiers >= level {
-							continue
-						}
-					}
-				}
-
-				if i%4 == 0 {
-					pdf.Ln(10.2)
-				}
-				s := fmt.Sprintf("%-2d %s %-2d = ", a, ops[o], b)
-				pdf.CellFormat(49, 10, s, "", 0, "L", false, 0, "")
-				break
-			}
-		}
+	pdf := NewFpdf("noto", "mono", "times", 16)
+	if maximum == 100 {
+		WritePages100(pdf, pages, maximum, level)
 	}
 	f, err := ioutil.TempFile(os.TempDir(), "e100*.pdf")
 	if err != nil {
--- cmd/riemann/riemann.go
+++ cmd/riemann/riemann.go
@@ -3,11 +3,33 @@ package main
 import (
 	"bytes"
 	"compress/gzip"
+	"fmt"
 	"io/ioutil"
+	"math/rand"
+	"strings"
+
+	"github.com/jung-kurt/gofpdf"
 )
 
-func FontBytes() (b []byte) {
-	in := res["font"]
+var (
+	options = gofpdf.ImageOptions{ImageType: "png", ReadDpi: true, AllowNegativePosition: false}
+)
+
+func Names(resType string) (names []string) {
+	for key := range res {
+		if strings.HasPrefix(key, resType) {
+			name := strings.TrimPrefix(key, resType)
+			names = append(names, name)
+		}
+	}
+	return
+}
+
+func FontBytes(name string) (b []byte) {
+	if !strings.HasPrefix(name, "font.") {
+		name = fmt.Sprintf("font.%s", name)
+	}
+	in := res[name]
 	r, err := gzip.NewReader(bytes.NewBuffer(in))
 	if err != nil {
 		return
@@ -18,4 +40,116 @@ func FontBytes() (b []byte) {
 		return
 	}
 	return
+}
+
+func LoadFonts(pdf *gofpdf.Fpdf) {
+	for _, fn := range Names("font.") {
+		pdf.AddUTF8FontFromBytes(fn, "", FontBytes(fn))
+	}
+}
+
+func RegisterPngImages(pdf *gofpdf.Fpdf) {
+	for _, png := range Names("png.") {
+		pdf.RegisterImageOptionsReader(png, options, bytes.NewBuffer(res[fmt.Sprintf("png.%s", png)]))
+	}
+}
+
+func NewFpdf(header, footer, body string, fsize float64) (pdf *gofpdf.Fpdf) {
+	pdf = gofpdf.New("P", "mm", "A4", "")
+	LoadFonts(pdf)
+	RegisterPngImages(pdf)
+	pdf.SetLeftMargin(12.5)
+	pdf.SetTopMargin(7.5)
+	pdf.SetHeaderFuncMode(func() {
+		pdf.SetFont(header, "", fsize)
+		pdf.Cell(50, 10, "姓名：")
+		pdf.Cell(48, 10, "日期：")
+		pdf.Cell(46, 10, "时间：")
+		pdf.Cell(25, 10, "分数：")
+		pdf.ImageOptions("riemann", -1, 6, 10, 10, false, options, 0, "")
+		width, _ := pdf.GetPageSize()
+		pdf.Line(10, 20, width-10, 20)
+		pdf.SetFont(body, "", fsize)
+	}, true)
+	pdf.SetFooterFunc(func() {
+		pdf.SetFont(footer, "", fsize)
+		pdf.SetY(-15)
+		width, _ := pdf.GetPageSize()
+		pdf.SetX(width/2 - 5)
+		pdf.Cell(10, 10, fmt.Sprintf("%-2d", pdf.PageNo()))
+	})
+	return
+}
+
+func carry(a, b int) bool {
+	a = a % 10
+	b = b % 10
+	if a+b >= 10 {
+		return true
+	}
+	return false
+}
+
+func borrow(a, b int) bool {
+	a = a % 10
+	b = b % 10
+	if a-b < 0 {
+		return true
+	}
+	return false
+}
+
+func WritePages100(pdf *gofpdf.Fpdf, pages, maximum, level int) {
+	ops := map[int]string{0: "+", 1: "-"}
+	for p := 0; p < pages; p++ {
+		pdf.AddPage()
+		pdf.Ln(3.5)
+		easiers := 0
+		for i := 0; i < maximum; i++ {
+			var a, b, c, o int
+			for {
+				a = rand.Intn(maximum)
+				b = rand.Intn(maximum)
+				if a < 10 || b < 10 {
+					easiers++
+					if easiers >= level {
+						continue
+					}
+				}
+
+				c = 0
+				o = rand.Intn(2)
+				if o == 0 {
+					c = a + b
+					if c > maximum {
+						continue
+					}
+					if !carry(a, b) {
+						easiers++
+						if easiers >= level {
+							continue
+						}
+					}
+				} else {
+					c = a - b
+					if c < 0 {
+						continue
+					}
+					if !borrow(a, b) {
+						easiers++
+						if easiers >= level {
+							continue
+						}
+					}
+				}
+
+				if i%4 == 0 {
+					pdf.Ln(10.2)
+				}
+				s := fmt.Sprintf("%-2d %s %-2d = ", a, ops[o], b)
+				pdf.CellFormat(49, 10, s, "", 0, "L", false, 0, "")
+				break
+			}
+		}
+	}
 }
